dfb6d935c0a10b67a4b82520ff10e9a300ed8512
skillink972 <79556664+skillink972@users.noreply.github.com>
2023-03-12 01:42:16 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+    <header>
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#news">log in/Sing up</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li style="float:right"><a class="active" href="#about">About</a></li>
+        </ul>
+    </header>
+
+</head>
+<body>
+<h1>TEST</h1>
+</body>
+</html>
